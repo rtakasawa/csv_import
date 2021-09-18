fd67be2fd9376e9a3fa06f21933c6890b9b80711
ryosuke takasawa <r.takasawa@wiz-net.jp>
2021-09-18 09:41:06 +0900

    add csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
 
   def self.import(file)
 
-    #<CSV::Row "id":"1" "name":"1,2" "age":"60" "status":"normal">
-    # => #<CSV::Table mode:col_or_row row_count:2>
+    # csvのheaderをカラムの用に出力する方法
     #   irb(User):026:0> csv = CSV.read(file,headers: :first_row,)
     # => #<CSV::Table mode:col_or_row row_count:2>
     #   irb(User):027:1* csv.each do |row|
@@ -16,8 +15,13 @@ class User < ApplicationRecord
     # csvのヘッダーのみ読込
     csv_header = CSV.read(file).first
 
+
+
     CSV.foreach(file.path, headers: true) do |row|
+
+      # csvの値を変更する方法
       # row["status"] = row["status"].to_i
+
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
       user = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
